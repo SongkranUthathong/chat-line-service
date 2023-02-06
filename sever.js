928fadfe1e0ea7df83ebb3c405ea5b087e449697
SongkranUthathong <songkran.u.github@gmail.com>
2023-02-06 17:27:45 +0700

    Update Get Weather2

--- sever.js
+++ sever.js
@@ -7,8 +7,23 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/',(req,res)=>{
+        let messageNotification = ""
+    request.get({
+        url: 'https://api.openweathermap.org/data/2.5/weather?lat=13.71175516&lon=100.59223652&appid=63f338f24c40bb032d4faa52470c5b6c',
+    }, (err, res, body) => {
+        var weather = JSON.parse(body);
+        messageNotification = "สวัสดีตอนเช้าครับ\n"+
+                   "ตอนนี้เวลา : "+getDateTime() +"\n"+
+                    "สภาพอากาศ : "+weather.weather[0].description + " ปริมาณเมฆ : "+weather.clouds.all+ " %\n"+
+                    "อุณหภูมิตอนนี้ : "+(weather.main.temp-273.15).toFixed(1)+ " C" +" | อุณหภูมิต่ำสุด : "+(weather.main.temp_min-273.15).toFixed(1)+ " C" +" | อุณหภูมิสูงสุด : "+(weather.main.temp_max-273.15).toFixed(1)+ " C" + "\n"+
+                    "ความชื้น : " + weather.main.humidity+ " %" +" | ความกดอากาศ : "+weather.main.pressure +" hPa" +"\n"+
+                    "ความเร็วลม : " + weather.wind.speed + "m/s , "+ "ทิศทางลม : "+ weather.wind.deg + " Degree\n"+
+                    "API:OpenWeather";
+                    console.log(messageNotification);
+    });
     res.json({
-        message:"Hello Welcome My Web Service!!!"
+        
+        message:"Hello Welcome My Web Service!!!\n" + messageNotification
     })
 });
 
