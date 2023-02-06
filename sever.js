e6a11edbc5688b3262f6fd70b36e0a4163cc9f60
SongkranUthathong <songkran.u.github@gmail.com>
2023-02-06 17:12:02 +0700

    first commit1

--- sever.js
+++ sever.js
@@ -28,21 +28,9 @@ app.listen(port, () => {
   function reply(reply_token) {
     let headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer {wQeoX+XQuie12rpBOgusJ3V/g/rxlHrqLIggGEWf1ev9noTDgWGpUpSYLBD/wFfH5nggaFzzRP30S7p2ECEt0ta6OOUfz6MFssCKfUPBEUFim32fj7vbrnv6OgXfSX8s+Ipj8RyLndcesZkgCvGUDwdB04t89/1O/w1cDnyilFU=}'
+        'Authorization': 'Bearer {RjT8hCJ6B9Pkn+KN1bhLBUjlNJtGnNrtZ9nf7cJ9hQ7nUKAnKDcccSuHThjmP1os5nggaFzzRP30S7p2ECEt0ta6OOUfz6MFssCKfUPBEUGmmOmT6Cbg5zk694vyVM80R/gnE7O06wxzHiC4dYEsiwdB04t89/1O/w1cDnyilFU=}'
     }
-    var messageNotification;
-    request.get({
-        url: 'https://api.openweathermap.org/data/2.5/weather?lat=13.71175516&lon=100.59223652&appid=63f338f24c40bb032d4faa52470c5b6c',
-    }, (err, res, body) => {
-        var weather = JSON.parse(body);
-        this.messageNotification = "สวัสดีตอนเช้าครับ\n"+
-                   "ตอนนี้เวลา : "+getDateTime() +"\n"+
-                    "สภาพอากาศ : "+weather.weather[0].description + " ปริมาณเมฆ : "+weather.clouds.all+ " %\n"+
-                    "อุณหภูมิตอนนี้ : "+(weather.main.temp-273.15).toFixed(1)+ " C" +" | อุณหภูมิต่ำสุด : "+(weather.main.temp_min-273.15).toFixed(1)+ " C" +" | อุณหภูมิสูงสุด : "+(weather.main.temp_max-273.15).toFixed(1)+ " C" + "\n"+
-                    "ความชื้น : " + weather.main.humidity+ " %" +" | ความกดอากาศ : "+weather.main.pressure +" hPa" +"\n"+
-                    "ความเร็วลม : " + weather.wind.speed + "m/s , "+ "ทิศทางลม : "+ weather.wind.deg + " Degree\n"+
-                    "API:OpenWeather";
-    });
+
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [
