3e848abf49898c192f45baf9b4f1a0825035ddb0
SongkranUthathong <songkran.u.github@gmail.com>
2023-02-08 02:26:38 +0700

    Update Aleart10

--- module/weatherNoti.js
+++ module/weatherNoti.js
@@ -1,19 +1,25 @@
 const request = require('../node_modules/request');
 process.env.TZ ="Asia/Bangkok"
-function getDateTime(){
-    const day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday ","Saturday"]
-    const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+function getDate(){
+    const day = ["Sun","Mon","Tue","Wed","Thu","Fri ","Sat"]
+    const month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+
+    let d = new Date();
+    let d_name = day[d.getDay()];
+    let m_name = month[d.getMonth()];
+    let date = d.getDate();
+    let year = d.getFullYear();
+    return d_name + " " + m_name + " " + date + " " + year;
+}
+function getTime(){
 
     let d = new Date();
     let hour = d.getHours();
     let minutes = d.getMinutes();
     let seconds = d.getSeconds();
-    let d_name = day[d.getDay()];
-    let m_name = month[d.getMonth()];
-    let date = d.getDate();
-    let year = d.getFullYear();
-    return d_name + " " + m_name + " " + date + " " + year +" " +addPoint(hour)+":" +addPoint(minutes)+ ":" +addPoint(seconds);
+    return addPoint(hour)+":" +addPoint(minutes)+ ":" +addPoint(seconds);
 }
+
 function addPoint(num){
     if(num.lenght < 2){
         return "0"+num;
@@ -29,8 +35,8 @@ function OpenWeather(cbfnc){
         url: 'https://api.openweathermap.org/data/2.5/weather?lat=13.71175516&lon=100.59223652&appid=63f338f24c40bb032d4faa52470c5b6c',
     }, (err, res, body) => {
         var weather = JSON.parse(body);
-        messageNotification = "สวัสดีตอนเช้าครับ  > Location : "+weather.name  +"\n"+
-                   "ตอนนี้เวลา : "+getDateTime() +"\n"+
+        messageNotification = "At : "+weather.name +" >" + getDate()+"\n"+
+                   "ตอนนี้เวลา : "+getTime() +"\n"+
                     "สภาพอากาศ : "+weather.weather[0].description + " ปริมาณเมฆ : "+weather.clouds.all+ " %\n"+
                     "อุณหภูมิตอนนี้ : "+(weather.main.temp-273.15).toFixed(1)+ " C" +"  อุณหภูมิต่ำสุด : "+(weather.main.temp_min-273.15).toFixed(1)+ " C" +"  อุณหภูมิสูงสุด : "+(weather.main.temp_max-273.15).toFixed(1)+ " C" + "\n"+
                     "ความชื้น : " + weather.main.humidity+ " %" +"  ความกดอากาศ : "+weather.main.pressure +" hPa" +"\n"+
@@ -40,5 +46,4 @@ function OpenWeather(cbfnc){
     });
 }
 
-exports.getTime= getDateTime;
 exports.showWeather= OpenWeather;
--- sever.js
+++ sever.js
@@ -1,12 +1,50 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const request = require('request')
+const schedule = require('node-schedule');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
 const weather = require('./module/weatherNoti');
+
+const rule = new schedule.RecurrenceRule();
+rule.dayOfWeek = [0, new schedule.Range(4, 6)];
+rule.hour = 2;
+rule.minute = 28;
+
+const job = schedule.scheduleJob(rule, function(){
+    weather.showWeather(function(data){
+
+    
+        let headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer {RjT8hCJ6B9Pkn+KN1bhLBUjlNJtGnNrtZ9nf7cJ9hQ7nUKAnKDcccSuHThjmP1os5nggaFzzRP30S7p2ECEt0ta6OOUfz6MFssCKfUPBEUGmmOmT6Cbg5zk694vyVM80R/gnE7O06wxzHiC4dYEsiwdB04t89/1O/w1cDnyilFU=}'
+        }
+        let body = JSON.stringify({
+            to: "Uce4a9dc86e6076e0abea7f3802bd77c9",
+            messages: [
+                {
+                    "type":"text",
+                    "text":"Good Morning Sir!!!"
+                },
+                {
+                    "type":"text",
+                    "text":data
+                }
+        ]
+        })
+        request.post({
+            url: 'https://api.line.me/v2/bot/message/push',
+            headers: headers,
+            body: body
+        }, (err, res, body) => {
+            console.log('status = ' + res.statusCode);
+        });
+    });
+  });
+
+
 app.get('/',(req,res)=>{
     
         res.json({ 
